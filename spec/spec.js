2f7c2561c150abde1b1669254e2d4fbd23d43650
Rodney Ossai <ossai2007@gmail.com>
2017-05-16 11:19:58 +0100

    First Commit

---
+++ spec/spec.js
@@ -0,0 +1,45 @@
+//My Testing code goes here
+
+describe("Edge Cases For prime Number Function", function(){ 
+   
+   it("should return an empty array for 1",function(){ 
+      expect(getPrimes(1)).toEqual([]); 
+   });
+
+   it("should return an empty array for 0",function(){ 
+      expect(getPrimes(0)).toEqual([]); 
+   });
+
+   it("should return 'Invalid Input' for a string",function(){ 
+      expect(getPrimes('b')).toEqual('Invalid Input'); 
+   });
+
+   it("should return 'Invalid Input' for a blank input",function(){ 
+      expect(getPrimes()).toEqual('Invalid Input'); 
+   });
+
+   it("should return 'Negative Numbers Not Allowed' for a negative integer",function(){ 
+      expect(getPrimes(-20)).toEqual('Negative Numbers Not Allowed'); 
+   });
+
+   it("should return [2] for 2",function(){ 
+      expect(getPrimes(2)).toEqual([2]); 
+   }); 
+
+   it("should return [2, 3] for 3",function(){ 
+      expect(getPrimes(3)).toEqual([2,3]); 
+   });
+
+   it("should return [2, 3] for 4",function(){ 
+      expect(getPrimes(4)).toEqual([2,3]); 
+   }); 
+
+   it("should return [ 2, 3, 5, 7, 11, 13, 17, 19 ] for 20",function(){ 
+      expect(getPrimes(20)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19 ]); 
+   }); 
+
+   it("should return [ 2, 3, 5, 7, 11] for 11",function(){ 
+      expect(getPrimes(11)).toEqual([ 2, 3, 5, 7, 11]); 
+   }); 
+
+});
---
+++ src/code.js
@@ -0,0 +1,30 @@
+let getPrimes = (n) => {
+  
+          if ((typeof n === 'string') || (n === undefined)) {
+            primeNumbers = "Invalid Input";
+        } else if (!(n > -1)) {
+            primeNumbers = "Negative Numbers Not Allowed";
+        } else {
+              let maxSize = n + 1 ;//Find all prime numbers less than the input
+              let numberList = []; //Initialize an empty array
+              primeNumbers =[];
+              //Exclude all even numbers from the checking except 2
+              numberList[2] = 1;
+              for(let i = 3; i < maxSize; i+=2){
+                numberList[i] = 1;
+              }
+              //Exclude all other non prime numbers
+              for(let i = 3; i < maxSize; i+=2){
+                for(let j = 3; i * j < maxSize; j+=2){
+                  numberList[i*j] = 0;
+                } 
+              }
+              // after excluding this two, what remaining must be a prime number so we display it
+              for(let i = 2; i < maxSize; i++){
+                  if (numberList[i]){
+                    primeNumbers.push(i);
+                  }
+              }
+        }
+return primeNumbers;
+};
